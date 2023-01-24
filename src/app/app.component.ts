49b064eadf2214fb90f0c9efbe84e867a331d8d4
Ginelle-19 <101726053+Ginelle-19@users.noreply.github.com>
2023-01-24 19:44:01 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,22 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // name = 'Angular ' + VERSION.major;
+  //INTERPOLATION
   appName =
     'This printout is an example of string interpolation using one-way data binding';
+  //PROPERTY BINDING
   clientName: string = 'Andrew';
+  // STYLE BINDING
   appliedCSSClass = 'green';
   notappliedCSSClass = false;
   myColor = 'red';
-
   myName = '';
+  // EVENT BINDING
+  showData($event: any) {
+    console.log('button is clicked!');
+    if ($event) {
+      console.log($event.target);
+      console.log($event.target.value);
+    }
+  }
 }
